5bbeae5e911e8ff5141082a0fd8cacd33c950d0c
ferjahm <ferjani.ahmed.2001@gmail.com>
2024-06-01 13:49:28 +0100

    implement the auth module and the auth startegy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from 'src/guards/auth-locale.guard';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    @Post("login")
-    login(){
 
+    constructor(private authService: AuthService){}
+
+    @UseGuards(LocalAuthGuard)
+    @Post("login")
+    async login(@Request() req){
+       return await this.authService.login(req.user);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get("profile")
-    getProfile(){
-
+    getProfile(@Request() req){
+        return req.user
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+      ){}
 
-    login(){}
+
+
+    async validateUser(username:string , pass : string){
+        const user : UserEntity = await (this.usersService.findByUsername(username) as unknown)as UserEntity;
+        if (user?.password === pass) {
+            const { password, ...result } = user;
+            return result;   
+        }else{
+            throw new UnauthorizedException("invalid username or password");
+        }
+    }
+
+    async login(user : UserEntity){
+        const payload = { _id: user._id, username: user.username , role : user.role};
+        return {
+          access_token: await this.jwtService.signAsync(payload),
+        };
+    }
 }
